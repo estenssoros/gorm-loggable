9969b9de84cd922b6c80236dcb98911a955b4ad9
estenssoros <seb.estenssoro@gmail.com>
2020-03-19 10:03:38 -0600

    feat: context stuff for plugin

--- plugin.go
+++ plugin.go
@@ -1,14 +1,17 @@
 package loggable
 
 import (
+	"context"
+
 	"github.com/jinzhu/gorm"
+	"github.com/pkg/errors"
 )
 
 // Plugin is a hook for gorm.
 type Plugin struct {
 	db      *gorm.DB
 	opts    options
-	context Context
+	Context Context
 	userKey string
 }
 
@@ -16,12 +19,27 @@ type Context interface {
 	Value(interface{}) interface{}
 }
 
+// RegisterContext validates and adds a context to the plugin
+func (p *Plugin) RegisterContext(ctx Context) error {
+	_, ok := ctx.Value(p.userKey).(string)
+	if !ok {
+		return errors.Errorf("missing %s on context", p.userKey)
+	}
+	p.Context = ctx
+	return nil
+}
+
+// defaultContext default context
+func defaultContext() Context {
+	return context.WithValue(context.Background(), "user", "")
+}
+
 var defaultUserKey = "user"
 
 // Register initializes Plugin for provided gorm.DB.
 // There is also available some options, that should be passed there.
 // Options cannot be set after initialization.
-func Register(db *gorm.DB, c Context, opts ...Option) (Plugin, error) {
+func Register(db *gorm.DB, opts ...Option) (Plugin, error) {
 	err := db.AutoMigrate(&ChangeLog{}).Error
 	if err != nil {
 		return Plugin{}, err
@@ -30,7 +48,7 @@ func Register(db *gorm.DB, c Context, opts ...Option) (Plugin, error) {
 	for _, option := range opts {
 		option(&o)
 	}
-	p := Plugin{db: db, opts: o, context: c, userKey: defaultUserKey}
+	p := Plugin{db: db, opts: o, Context: defaultContext(), userKey: defaultUserKey}
 	callback := db.Callback()
 	callback.Query().After("gorm:after_query").Register("loggable:query", p.trackEntity)
 	callback.Create().After("gorm:after_create").Register("loggable:create", p.addCreated)
